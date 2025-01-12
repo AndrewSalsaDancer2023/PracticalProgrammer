6153043ff829ae32a975253bad2cf7e380ca62cc
andrew <flyiq442@gmail.com>
2025-01-12 23:29:21 +0300

    initial commit

---
+++ calculator/src/logic/logic.cpp
@@ -0,0 +1,9 @@
+#include "logic.h"
+int bitwise_and(int op1, int op2)
+{ return op1 & op2; }
+int bitwise_or(int op1, int op2)
+{ return op1 | op2; }
+int bitwise_xor(int op1, int op2)
+{ return op1 ^ op2; }
+int bitwise_not(int op)
+{ return ~op; }
